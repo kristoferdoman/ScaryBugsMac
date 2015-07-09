96d1ea1faa6098cf091cf080c180204e65a48caf
Kristofer Doman <kristoferdoman@gmail.com>
2015-07-09 10:52:06 -0500

    Initial Commit

---
+++ ScaryBugsMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScaryBugsMac
+//
+//  Created by Kristofer Doman on 2015-07-09.
+//  Copyright (c) 2015 Kristofer Doman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
